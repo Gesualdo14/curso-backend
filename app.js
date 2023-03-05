a6f5e89eb16f0adba40b19b40a3ee962cb789f70
Martín Gesualdo <mgesudev@gmail.com>
2023-03-05 16:31:13 -0300

    Corregimos el tema del commit con node modules

--- app.js
+++ app.js
@@ -1,30 +1,49 @@
-const http = require("http")
-const os = require("os")
+const express = require("express")
+const cors = require("cors")
+const { dbConnect, Task } = require("./database")
 
-const server = http.createServer((req, res) => {
-  console.log({ method: req.method, url: req.url, origin: req.headers.origin })
-  if (
-    ["https://curso-mu.vercel.app", "http://127.0.0.1:5500"].includes(
-      req.headers.origin
-    )
-  ) {
-    res.setHeader("Access-Control-Allow-Origin", req.headers.origin)
-  }
-  res.setHeader("status", 200)
-  res.setHeader("Content-Type", "application/json")
-  res.write(
-    JSON.stringify({
-      ok: true,
-      message: `Hola soy una compu, mi nombre es ${os.hostname()} y recibí tu petición a la ruta '${
-        req.url
-      }'`,
-    })
-  )
-  res.end()
+dbConnect()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find()
+
+  res.status(200).json({
+    ok: true,
+    message: "Tarea creada con éxito ✅",
+    data: tasks,
+  })
+})
+
+app.post("/tasks", async (req, res) => {
+  console.log({ body: req.body })
+
+  const createdTask = await Task.create({ name: req.body.taskName })
+
+  res.status(201).json({
+    ok: true,
+    message: "Tarea creada con éxito ✅",
+    data: createdTask._id,
+  })
+})
+
+app.delete("/tasks/:id", async (req, res) => {
+  const { id } = req.params
+
+  await Task.findByIdAndRemove(id)
+
+  res.status(200).json({
+    ok: true,
+    message: "Tarea eliminada con éxito ✅",
+  })
 })
 
 const PORT = process.env.PORT || 4000
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Computadora escuchando peticiones en el puerto ${PORT}`)
 })
