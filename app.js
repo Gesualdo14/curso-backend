2ccb81dbc9d6c21900ee27ef91dab823ebed266a
Martín Gesualdo <mgesudev@gmail.com>
2023-03-05 18:12:07 -0300

    Ruta para subir archivos

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express")
 const cors = require("cors")
 const { dbConnect, Task } = require("./database")
+const fileUpload = require("express-fileupload")
+
+const { BlobServiceClient } = require("@azure/storage-blob")
+
+// Configurar el cliente de Azure Blob Storage
+const connectionString =
+  "DefaultEndpointsProtocol=https;AccountName=tuturno;AccountKey=ERq1mkptZ15nImMnVSvrJYWMXXunCW5s+6pQBvsGv9uuFDa+WIC4emQN6it/gx2v5BOfUxYthLR8+AStr1RC4A==;EndpointSuffix=core.windows.net"
+const blobServiceClient =
+  BlobServiceClient.fromConnectionString(connectionString)
+const containerName = "tuturno"
+const containerClient = blobServiceClient.getContainerClient(containerName)
 
 dbConnect()
 
@@ -8,6 +19,12 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+// Configurar el middleware fileupload
+app.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 }, // límite de tamaño del archivo en bytes
+  })
+)
 
 app.get("/tasks", async (req, res) => {
   const tasks = await Task.find()
@@ -29,6 +46,23 @@ app.post("/tasks", async (req, res) => {
     message: "Tarea creada con éxito ✅",
     data: createdTask._id,
   })
+})
+app.post("/tasks/:id/attach", async (req, res) => {
+  const { id } = req.params
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No se ha enviado ningún archivo")
+  }
+
+  // Obtener el archivo del objeto req.files
+  const file = req.files.file
+
+  // Subir el archivo a Azure Blob Storage
+  const blockBlobClient = containerClient.getBlockBlobClient(`${id}.png`)
+  await blockBlobClient.upload(file.data, file.data.length, {
+    blobHTTPHeaders: { blobContentType: file.mimetype },
+  })
+
+  res.json({ ok: true, data: "Archivo subido correctamente" })
 })
 
 app.delete("/tasks/:id", async (req, res) => {
